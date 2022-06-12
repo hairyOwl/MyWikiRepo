9b3c7735a1d44fb34ce91f40f965fb0eb02b803d
hairyOwl <lxy_happy@126.com>
2022-06-12 16:18:22 +0800

    2-7 增加通用返回类CommonResp,统一接口返回体

--- src/main/java/com/owl/wiki/controller/EbookController.java
+++ src/main/java/com/owl/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.owl.wiki.controller;
 
 import com.owl.wiki.domain.Ebook;
+import com.owl.wiki.resp.CommonResp;
 import com.owl.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,11 @@ public class EbookController {
     private EbookService ebookService; //ebookService
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp<List<Ebook>> list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> eList = ebookService.list();
+        //resp.setSuccess(true); //由于默认是true就不设置
+        resp.setContent(eList);
+        return resp;
     }
 }
---
+++ src/main/java/com/owl/wiki/resp/CommonResp.java
@@ -0,0 +1,49 @@
+package com.owl.wiki.resp;
+
+/**
+ * @description: 通用返回体
+ * @version: 1.0
+ * @author: 小哈
+ * @date: 2022/6/12 16:10
+ */
+
+public class CommonResp<T> {
+
+    private boolean success = true; //业务上的成功或失败
+    private String message; //返回信息
+    private T content; //返回泛型数据，自定义类型
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("ResponseDto{");
+        sb.append("success=").append(success);
+        sb.append(", message='").append(message).append('\'');
+        sb.append(", content=").append(content);
+        sb.append('}');
+        return sb.toString();
+    }
+}
