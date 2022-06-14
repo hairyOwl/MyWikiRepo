3aff380765edbdf0c82c15e21d7861e6a6b5c277
hairyOwl <lxy_happy@126.com>
2022-06-14 16:01:30 +0800

    3-3 网站首页布局

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -3,7 +3,7 @@
  * @Author: hairyOwl
  * @Date: 2022-06-13 15:46:23
  * @LastEditors: hairyOwl
- * @LastEditTime: 2022-06-13 22:32:58
+ * @LastEditTime: 2022-06-14 15:58:24
  */
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
@@ -14,14 +14,11 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited. 懒加载，只有访问了这个页面才加载
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = createRouter({
