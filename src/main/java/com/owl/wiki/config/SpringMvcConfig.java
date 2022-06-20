c0644e2ef237645832b8a19315a4e152c4f57bd6
hairyOwl <lxy_happy@126.com>
2022-06-20 15:59:00 +0800

    4-8 增加拦截器，打印接口耗时

---
+++ src/main/java/com/owl/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,27 @@
+package com.owl.wiki.config;
+
+import com.owl.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @description: 拦截器配置
+ * @version: 1.0
+ * @author: 小哈
+ * @date: 2022/6/20 15:33
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor; //注入拦截器
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor) //注册拦截器
+                .addPathPatterns("/**"); //针对所有的请求使用logInterceptor
+    }
+}
