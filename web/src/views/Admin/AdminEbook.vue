d9d3f64561623475c7db06b9e55d232c56863160
hairyOwl <lxy_happy@126.com>
2022-06-27 20:51:06 +0800

    5-2 修改电子书管理布局修改

--- web/src/views/Admin/AdminEbook.vue
+++ web/src/views/Admin/AdminEbook.vue
@@ -3,20 +3,29 @@
  * @Author: hairyOwl
  * @Date: 2022-06-25 10:09:16
  * @LastEditors: hairyOwl
- * @LastEditTime: 2022-06-25 10:17:01
+ * @LastEditTime: 2022-06-27 20:48:38
 -->
 <template>
-    <div>
-        电子书管理
-    </div>
+    <a-layout>
+        <!-- 内容 -->
+        <a-layout-content
+            :style="{
+                background: '#fff',
+                padding: '24px',
+                margin: 0,
+                minHeight: '280px',
+            }"
+        >
+        <div>
+            <h1>电子书管理</h1>
+        </div>
+        </a-layout-content>
+    </a-layout>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
